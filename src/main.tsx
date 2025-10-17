8b064037da315746837541da24f26d7b5267e37f
Hojiakbar <ibragimvich2201@gmail.com>
2025-10-17 15:22:52 +0500

    Vercel added

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import '@ant-design/v5-patch-for-react-19';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
